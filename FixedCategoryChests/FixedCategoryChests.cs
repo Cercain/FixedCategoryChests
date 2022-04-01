d459134ff9541f23e5ccab5e76e1b6358c2803f7
Cercain <Lilly.varoux@gmail.com>
2022-04-01 14:51:58 +0100

    Initial Commit

---
+++ FixedCategoryChests/FixedCategoryChests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using RoR2.Artifacts;
+using UnityEngine;
+using UnityEngine.Networking;
+using PickupIndex = RoR2.PickupIndex;
+
+namespace FixedCategoryChests
+{
+    [BepInDependency(R2API.R2API.PluginGUID)]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+    public class FixedCategoryChests : BaseUnityPlugin
+    {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "Cercain";
+        public const string PluginName = "FixedCategoryChests";
+        public const string PluginVersion = "1.0.0";
+
+        public void OnEnable()
+        {
+            On.RoR2.BasicPickupDropTable.PassesFilter += PassesFilter;
+        }
+
+        public void OnDisable()
+        {
+            On.RoR2.BasicPickupDropTable.PassesFilter -= PassesFilter;
+        }
+
+        bool PassesFilter(On.RoR2.BasicPickupDropTable.orig_PassesFilter orig, RoR2.BasicPickupDropTable self, PickupIndex pickupIndex)
+        {
+            PickupDef pickupDef = PickupCatalog.GetPickupDef(pickupIndex);
+
+            if (pickupDef.itemIndex != ItemIndex.None)
+            {
+                ItemTag[] tags = ItemCatalog.GetItemDef(pickupDef.itemIndex).tags;
+                foreach (var bannedTag in self.bannedItemTags)
+                    if (Array.IndexOf(tags, bannedTag) != -1)
+                        return false;
+
+                foreach (var requiredTag in self.requiredItemTags)
+                    if (Array.IndexOf(tags, requiredTag) == -1)
+                        return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ FixedCategoryChests/README.md
@@ -0,0 +1,13 @@
+## Fixed Chests
+
+So it turns out that category chests only populate their Drop Table with items that ONLY have the same tag
+For example Healing Chests can only drop: Slugs, Medkits, Scythe, Seed, Antlers
+This fixes that.
+
+Note: This is a bug that is due to be fixed next patch
+
+## Changelog
+
+**1.0.0**
+
+* Initial Release
